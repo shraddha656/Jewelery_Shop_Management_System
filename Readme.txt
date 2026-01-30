468414872171a5acf8a467eacf4d56a8944eb124
Shraddha Patange <spatange2004@gmail.com>
2026-01-30 15:42:34 +0530

    initial project upload

---
+++ Readme.txt
@@ -0,0 +1,29 @@
+How to run the Jewelery Shop Management System (JSMS) Project
+
+1. Download the  zip file
+
+2. Extract the file and copy jsms folder
+
+3.Paste inside root directory(for xampp xampp/htdocs, for wamp wamp/www, for lamp var/www/html)
+
+4. Open PHPMyAdmin (http://localhost/phpmyadmin)
+
+5. Create a database with name jsmsdb
+
+6. Import jsmsdb.sql file(given inside the zip package in SQL file folder)
+
+7.Run the script http://localhost/jsms (frontend)
+
+
+
+Credential for admin panel :
+
+Username: admin
+Password: Test@123
+
+Credential for  User panel :
+
+Username: johdoe@gmail.com
+Password: Test@123
+
+ Or Register a new User.
---
+++ jsms/includes/header.php
@@ -0,0 +1,44 @@
+<header id="header">
+        <div class="container">
+            <strong><a href="index.php">Jewellery Shop Management System</a></strong>
+            <div class="right-links">
+                <ul>
+                     <?php if (strlen($_SESSION['jsmsuid']>0)) {?>
+                    <li> <?php
+                            $userid= $_SESSION['jsmsuid'];
+$ret2=mysqli_query($con,"select sum(products.shippingCharge+products.productPrice) as total,COUNT(orders.PId) as totalp from orders join products on products.id=orders.PId where orders.UserId='$userid' and orders.IsOrderPlaced is null");
+$resultss=mysqli_fetch_array($ret2);
+
+?>
+                        <a href="cart.php"><span class="ico-products"></span><?php echo $resultss['totalp'];?> products, $<?php echo $resultss['total'];?></a></li>
+                   <li><a href="wishlist.php"><span class="ico-heart"></span>Wishlist</a></li>
+                    <li><a href="profile.php"><span class="ico-account"></span>My Profile</a></li>
+                    <li><a href="my-orders.php"><span class="ico-cart"></span>My Orders</a></li>
+                    <li><a href="change-password.php"><span class="ico-signout"></span>Setting</a></li>
+                    <li><a href="logout.php"><span class="ico-signout"></span>Sign out</a></li><?php }?>
+                </ul>
+            </div>
+        </div>
+        <!-- / container -->
+    </header>
+    <!-- / header -->
+
+    <nav id="menu">
+        <div class="container">
+            <div class="trigger"></div>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                 <li><a href="about.php">About Us</a></li>
+                  <li><a href="products.php">Products</a></li>
+                <li><a href="category.php">Category</a></li>
+               
+               
+                <li><a href="contact.php">Contact Us</a></li>
+                <?php if (strlen($_SESSION['jsmsuid']==0)) {?>
+                <li><a href="signup.php">Register</a></li>
+                <li><a href="login.php">Login</a></li><?php }?>
+            </ul>
+        </div>
+        <!-- / container -->
+    </nav>
+    <!-- / navigation -->
